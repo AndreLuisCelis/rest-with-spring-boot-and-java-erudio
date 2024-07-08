d3ef5838f745688d2284d9dfc569a3fc99c1c1d7
Andre Luis Celis <andre.celis@meta.com.br>
2024-07-08 16:25:58 -0300

    json custom serialization

--- 06_PatchParams/rest-with-spring-boot-and-java-erudio/src/main/java/com/celisapp/data/vo/v1/PersonVO.java
+++ 06_PatchParams/rest-with-spring-boot-and-java-erudio/src/main/java/com/celisapp/data/vo/v1/PersonVO.java
@@ -3,14 +3,23 @@ package com.celisapp.data.vo.v1;
 import java.io.Serializable;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
+
+@JsonPropertyOrder({"id","address","first_name","last_name","gender"})
 public class PersonVO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	
 	private Long id;
+	
+	@JsonProperty("first_name")
 	private String firstName;
+	
+	@JsonProperty("last_name")
 	private String lastName;
+	
 	private String gender;
 	private String address;
 	
