11eccdeef5a757557b8d16cec505c149e65dfd8b
Andre Luis Celis <andre.celis@meta.com.br>
2024-07-17 17:39:30 -0300

    added upload and download files

--- 06_PatchParams/rest-with-spring-boot-and-java-erudio/src/main/java/com/celisapp/exceptions/MyFileNotFoundException.java
+++ 06_PatchParams/rest-with-spring-boot-and-java-erudio/src/main/java/com/celisapp/exceptions/MyFileNotFoundException.java
@@ -3,15 +3,15 @@ package com.celisapp.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-public class ResourceNotfoundException extends RuntimeException {
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class MyFileNotFoundException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 
-	public ResourceNotfoundException(String ex) {
+	public MyFileNotFoundException(String ex) {
 		super(ex);
 	}
 	
-	public ResourceNotfoundException(String ex, Throwable cause) {
+	public MyFileNotFoundException(String ex, Throwable cause) {
 		super(ex, cause);
 	}
 
